592078b5629e6393892fc188e1d2ade1f97ae0f4
Denis <den1980@gmail.com>
2015-01-29 23:34:45 +0300

    Created file for document model

---
+++ Tasks/TaskDocument.swift
@@ -0,0 +1,13 @@
+//
+//  TaskDocument.swift
+//  Tasks
+//
+//  Created by Денис Львович on 29.01.15.
+//  Copyright (c) 2015 Денис Львович. All rights reserved.
+//
+
+import UIKit
+
+class TaskDocument: UIDocument {
+   
+}
